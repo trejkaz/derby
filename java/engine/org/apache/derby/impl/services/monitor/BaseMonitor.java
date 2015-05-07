df866b53ab4f4b00c433552c6d45918c58c8e5db
Trejkaz (pen name) <trejkaz@trypticon.org>
2015-05-07 10:00:04 +1000

    Fixes DERBY-6796 - cutting the wrong prefix length when parsing the subSubProtocol

--- java/engine/org/apache/derby/impl/services/monitor/BaseMonitor.java
+++ java/engine/org/apache/derby/impl/services/monitor/BaseMonitor.java
@@ -1079,7 +1079,7 @@ nextModule:
 			if (key.startsWith(Property.MODULE_PREFIX)) {
 				tag = key.substring(Property.MODULE_PREFIX.length());
             } else if (key.startsWith(Property.SUB_SUB_PROTOCOL_PREFIX)) {
-                tag = key.substring(Property.MODULE_PREFIX.length());
+                tag = key.substring(Property.SUB_SUB_PROTOCOL_PREFIX.length());
             } else {
                 continue nextModule;
             }
