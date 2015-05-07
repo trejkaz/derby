2c04ccf923d6dec39ddc5734a33e3f26adc26370
Trejkaz (pen name) <trejkaz@trypticon.org>
2015-05-07 15:41:47 +1000

    Fixing merge issues breaking compilation

--- java/engine/org/apache/derby/impl/store/raw/log/FileLogger.java
+++ java/engine/org/apache/derby/impl/store/raw/log/FileLogger.java
@@ -52,7 +52,7 @@ import org.apache.derby.iapi.services.io.FormatIdOutputStream;
 
 import org.apache.derby.iapi.services.monitor.Monitor;
 
-import org.apache.derby.iapi.services.sanity.SanityManager;
+import org.apache.derby.shared.common.sanity.SanityManager;
 
 import org.apache.derby.iapi.error.StandardException;
 import org.apache.derby.iapi.services.i18n.MessageService;
